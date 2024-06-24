38b5d85758c757289f382f1d37ca4c4223cea42d
Jared Clyde <jarclyde@gmail.com>
2024-06-24 15:27:59 -0600

    Created level, added laser sight, added goal and win screen.

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    [SerializeField]
+    GameObject endScreen;
+
+    [SerializeField]
+    GameObject player,
+        portalGun;
+
+    bool gameOver = false;
+
+    // if the player has collided with the goal, activate the end screen, disable the gun, and activate the cursor
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collided with goal");
+
+        if (other.gameObject == player)
+        {
+            gameOver = true;
+
+            // player.transform.position = new Vector3(5, 2, 3);
+
+            endScreen.SetActive(true);
+            // portalGun.SetActive(false);
+			player.SetActive(false);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (gameOver)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+}
